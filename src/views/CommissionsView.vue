59e3bc7962988874746398f1532c84fba9219d6d
luckybluejay27 <luckybluejay27@gmail.com>
2025-03-16 03:00:52 -0700

    Prev state from accidental del

---
+++ src/views/CommissionsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <h1>Commish</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Commissions"
+});
+</script>
---
+++ src/views/ContractView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <h1>Contracting</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Contracting"
+});
+</script>
